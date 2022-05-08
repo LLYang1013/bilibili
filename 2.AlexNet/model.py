17074d2b2c0635e59847e82c45f5491b50167622
LLYang1013 <1193835379@qq.com>
2022-05-08 21:37:28 +0800

    alex

---
+++ 2.AlexNet/model.py
@@ -0,0 +1,51 @@
+import torch.nn as nn
+import torch
+
+
+class AlexNet(nn.Module):
+    def __init__(self, num_classes=1000, init_weights=False):
+        super(AlexNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 48, kernel_size=11, stride=4, padding=2),  # input[3, 224, 224]  output[48, 55, 55]
+            # 由于本次训练的数据集较小，所以卷积核个数改为原来的一半48，加快训练速度
+            # 这里为了方便，上下左右各补2，224-11+4)/4 +1 =55.25, 向下取整55
+            nn.ReLU(inplace=True),  # 不创建新的对象，直接对原始对象进行修改，节省内存
+            nn.MaxPool2d(kernel_size=3, stride=2),                  # output[48, 27, 27]
+            nn.Conv2d(48, 128, kernel_size=5, padding=2),           # output[128, 27, 27]
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),                  # output[128, 13, 13]
+            nn.Conv2d(128, 192, kernel_size=3, padding=1),          # output[192, 13, 13]
+            nn.ReLU(inplace=True),
+            nn.Conv2d(192, 192, kernel_size=3, padding=1),          # output[192, 13, 13]
+            nn.ReLU(inplace=True),
+            nn.Conv2d(192, 128, kernel_size=3, padding=1),          # output[128, 13, 13]
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),                  # output[128, 6, 6]
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=0.5),
+            nn.Linear(128 * 6 * 6, 2048),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5),
+            nn.Linear(2048, 2048),
+            nn.ReLU(inplace=True),
+            nn.Linear(2048, num_classes),  # 这里是5类
+        )
+        if init_weights:
+            self._initialize_weights()
+
+    def forward(self, x):  # 正向传播
+        x = self.features(x)
+        x = torch.flatten(x, start_dim=1)  # 从channel开始展平，h，w
+        x = self.classifier(x)
+        return x
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
